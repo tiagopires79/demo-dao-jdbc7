f31dea9abf8b07a4e076cb7f16b0c5dc017e4c0b
tiagopires79 <tiagopiresdb@gmail.com>
2020-10-12 16:09:52 -0300

    findall Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 			 System.out.println(obj);
 		}
 		
+		System.out.println("\n### TEST3: Seller findAll ###");
+		listSeller = sellerDao.findAll();
+		for(Seller obj : listSeller) {
+			 System.out.println(obj);
+		}
+		
 	}
 
 }
